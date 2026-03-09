9951400f14e84a4da3cc03f23fe1c7df0e3a3f57
Zonghao Zhang <azongchang@qq.com>
2026-03-09 20:34:21 +0800

    Add maximum unlink files

--- src/MWUL.c
+++ src/MWUL.c
@@ -18,7 +18,7 @@
 #include "util.h"
 #include "rdtsc.h"
 
-#define MWUL_FILES_PER_WORKER 1000000ULL
+#define MWUL_FILES_PER_WORKER 3000000ULL
 
 static void set_test_root(struct worker *worker, char *test_root) {
     struct fx_opt *fx_opt = fx_opt_worker(worker);
